80215281b57aa816f3c37c7b3be50ff2365e00a4
Kusya Lakomkin <zubareff.4lex@yandex.ru>
2024-11-13 17:38:22 +0300

    Added test to rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,4 @@
+import unittest
 def area(a,b):
     '''
             Возвращает произведение двух десятичных чисел.
@@ -21,3 +22,26 @@ def perimeter(a,b):
                                perimeter (float): периметр прямоугольника со сторонами a и b
         '''
     return (a+b)*2
+
+class RectangleTestCase(unittest.TestCase):
+    def test_sides_ten_five_area(self):
+        res = area(10, 5)
+        self.assertEqual(res, 50)
+
+    def test_sides_two_one_and_half_area(self):
+        res = area(2, 1.5)
+        self.assertEqual(res, 3)
+
+    def test_invalid_area(self):
+       self.assertRaises(Exception, lambda: area(-1, 10))
+
+    def test_sides_five_six_perimeter(self):
+        res = perimeter(5, 6)
+        self.assertEqual(res, 22)
+
+    def test_sides_one_and_half_perimeter(self):
+       res = perimeter(1.5, 1.5)
+       self.assertEqual(res, 6)
+
+    def test_invalid_perimeter(self):
+        self.assertRaises(Exception, lambda: perimeter(-1, 10))
